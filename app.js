307625e111d95411e0693e3a8cd755adeb5d9fc8
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-08-29 15:57:42 +0530

    feat : setting up edit route and ejs page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/sweetspot";
 
@@ -17,6 +18,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 
 const port = 8080;
@@ -66,5 +68,18 @@ app.post("/listings", async (req, res) => {
   console.log(listing);
   const newListing = new Listing(listing);
   await newListing.save();
+  res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  console.log(listing);
+  res.render("listings/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect("/listings");
 });
